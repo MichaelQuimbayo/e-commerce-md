9add47a0eb0e21dbf4be7193cbae1c68985d8490
Michael Quimbayo <developermike18@gmail.com>
2026-02-27 14:32:42 -0500

    control color y tallas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca40636426ab952f"; }
+/******/ 		__webpack_require__.h = function() { return "dc32a2a067735914"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
